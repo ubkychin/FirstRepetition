e6ccb4c6af0583344c99eb9fa45aed908b1669a2
Anh Nguyen <anguyen@swin.edu.au>
2022-10-05 11:18:19 +1100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+var incrementer = 0;
+var stopValue = 10;
+
+// Repetition
+while (incrementer <= stopValue) {
+    System.Console.WriteLine(incrementer);
+    incrementer++;
+}
